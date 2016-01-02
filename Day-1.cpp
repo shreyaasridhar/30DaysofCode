e80f80898c2c6e3f3b0b9d2fcb870b51cad41ade
shreyaasridhar <shreyanju@gmail.com>
2016-01-02 21:46:17 +0530

    adding

---
+++ Day-1.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    cout<<"Primitive : double\nPrimitive : char\nPrimitive : boolean\nPrimitive : int\nReferenced : String\nPrimitive : boolean\nPrimitive : double\nPrimitive : char\nReferenced : String";
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
+
